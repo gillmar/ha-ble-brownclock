7ebe045d750c3c973338a95f30b8aec4d5e60b62
gillmar <16834898+gillmar@users.noreply.github.com>
2026-07-20 19:51:41 +0200

    Update const.py

--- custom_components/braun_ble_alarm_clock/const.py
+++ custom_components/braun_ble_alarm_clock/const.py
@@ -1,7 +1,6 @@
 DOMAIN = "braun_bc20"
 UPDATE_INTERVAL = 60  # Sekunden
 
-# Diese UUIDs musst du aus deinem Reverse Engineering übernehmen:
-SERVICE_UUID = "F8083532-849E-531C-C594-30F1F86A4EA5"  # Beispiel
+SERVICE_UUID = "5A870001-0A96-2C87-4F9F-A22A719F0000"
 TIME_CHAR_UUID = "0000fff1-0000-1000-8000-00805f9b34fb"  # Beispiel
 ALARM_CHAR_UUID = "0000fff2-0000-1000-8000-00805f9b34fb"  # Beispiel
